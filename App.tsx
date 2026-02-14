b37a552f8d9a2702243ffc7487225f376cb25d46
abdozxcabdozxc5000-lab <abdozxcabdozxc5000@gmail.com>
2026-02-15 01:07:44 +0200

    feat: Initialize orders state as empty
    The initial list of hardcoded orders has been removed. The application will now start with an empty order list, allowing users to add new items.

--- App.tsx
+++ App.tsx
@@ -7,36 +7,7 @@ import { OrderItem } from './types';
 import { v4 as uuidv4 } from 'uuid';
 
 const App: React.FC = () => {
-  const [items, setItems] = useState<OrderItem[]>([
-    { id: '1', quantity: 20, name: 'وصلة ليد' },
-    { id: '2', quantity: 25, name: 'عود المونيوم بروفايل 1.7' },
-    { id: '3', quantity: 7, name: 'درايفر 400 وات' },
-    { id: '4', quantity: 2, name: 'درايفر 300 وات' },
-    { id: '5', quantity: 140, name: 'شاسيه' },
-    { id: '6', quantity: 50, name: 'مفتاح' },
-    { id: '7', quantity: 16, name: 'مفتاح دفتير' },
-    { id: '8', quantity: 50, name: 'لقمة بريزه' },
-    { id: '9', quantity: 25, name: 'بريزه مجوفه' },
-    { id: '10', quantity: 250, name: 'سداده' },
-    { id: '27', quantity: 1, name: 'بريزة دش' },
-    { id: '11', quantity: 2, name: 'بريزة تليفون' },
-    { id: '12', quantity: 14, name: 'مفتاح 2 فاز على علبه ماجيك' },
-    { id: '13', quantity: 1, name: 'مفتاح جرس' },
-    { id: '14', quantity: 1, name: 'جرس على علبه ماجيك' },
-    { id: '15', quantity: 2, name: 'باكو شريط لحام سويدى' },
-    { id: '16', quantity: 130, name: 'لمبة اسبوط' },
-    { id: '17', quantity: 65, name: 'اسبوط مجوز' },
-    { id: '18', quantity: 1, name: 'مفتاح ظبط التيار 3 فاز' },
-    { id: '19', quantity: 11, name: 'كشاف طوارئ مجوز' },
-    { id: '20', quantity: 8, name: 'مفتاح هاجر 32' },
-    { id: '21', quantity: 4, name: 'مفتاح 40 امبير هاجر' },
-    { id: '22', quantity: 1, name: 'مفتاح ايرث ليكيج 380 فولت هاجر' },
-    { id: '23', quantity: 10, name: 'مفتاح هاجر 25' },
-    { id: '24', quantity: 3, name: 'لمبة اشاره هاجر' },
-    { id: '25', quantity: 1, name: 'قاطع 100 امبير هاجر 3 فاز' },
-    { id: '28', quantity: 1, name: 'مفتاح دايمر' },
-    { id: '26', quantity: 3, name: 'مفتاح شطر' },
-  ]);
+  const [items, setItems] = useState<OrderItem[]>([]);
 
   const handleAddItem = (quantity: number, name: string) => {
     const newItem: OrderItem = {
